6c363fef7bc4febe0dc766ecfe645694f97456ff
Alexander Vordermaier <alexander.vordermaier@student.tugraz.at>
2023-01-31 17:41:30 +0100

    Type error in generate script

--- gen_imagenet_cartoon.sh
+++ gen_imagenet_cartoon.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=slurm-%J.err
 #SBATCH --partition=igi
 #SBATCH --nodelist=nvcluster-node5
-#SBATCH --ntask=1
+#SBATCH --ntasks=1
 
 cd src-imagenet-cartoon
 python3 --load_folder /home/a09v98/imagenet/validation --save_folder /home/a09v98/Projects/datasets/imagenet-cartoon
