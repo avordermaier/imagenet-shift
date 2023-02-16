c749e33f18c635c0765b23b6e3a7e468d4eed092
Alexander Vordermaier <alexander.vordermaier@student.tugraz.at>
2023-02-16 17:12:43 +0100

    Script to generate drawing 1 dataset

---
+++ gen_imagenet_drawing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexander.vordermaier@student.tugraz.at
+#SBATCH --output=slurm-%J.out
+#SBATCH --error=slurm-%J.err
+#SBATCH --partition=igi
+#SBATCH --nodelist=nvcluster-node5
+#SBATCH --ntasks=1
+
+cd src-imagenet-drawing
+python3 --load_folder /home/a09v98/imagenet/validation --save_folder /home/a09v98/Projects/data/imagenet-drawing
