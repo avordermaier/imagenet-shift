e9b64c1d84d3aaf236e5bdc1794a7bd936a5da0d
Alexander Vordermaier <alexander.vordermaier@student.tugraz.at>
2023-06-20 13:36:24 +0200

    Generate imagenet-drawing 2,3,4 train

--- gen_imagenet_drawing.sh
+++ gen_imagenet_drawing.sh
@@ -8,6 +8,6 @@
 #SBATCH --ntasks=1
 
 cd src-imagenet-drawing
-python3 drawing.py --load_folder /home/a09v98/imagenet/validation --save_folder /home/a09v98/Projects/data/imagenet-drawing2/validation --drawing-pattern /home/a09v98/Projects/imagenet-shift/src-imagenet-drawing/drawing-patterns/drawing-pattern-II.jpg
-python3 drawing.py --load_folder /home/a09v98/imagenet/validation --save_folder /home/a09v98/Projects/data/imagenet-drawing3/validation --drawing-pattern /home/a09v98/Projects/imagenet-shift/src-imagenet-drawing/drawing-patterns/drawing-pattern-III.png
-python3 drawing.py --load_folder /home/a09v98/imagenet/validation --save_folder /home/a09v98/Projects/data/imagenet-drawing4/validation --drawing-pattern /home/a09v98/Projects/imagenet-shift/src-imagenet-drawing/drawing-patterns/drawing-pattern-IV.jpg
+python3 drawing.py --load_folder /home/a09v98/imagenet/train --save_folder /home/a09v98/Projects/data/imagenet-drawing2/train --drawing-pattern /home/a09v98/Projects/imagenet-shift/src-imagenet-drawing/drawing-patterns/drawing-pattern-II.jpg
+python3 drawing.py --load_folder /home/a09v98/imagenet/train --save_folder /home/a09v98/Projects/data/imagenet-drawing3/train --drawing-pattern /home/a09v98/Projects/imagenet-shift/src-imagenet-drawing/drawing-patterns/drawing-pattern-III.png
+python3 drawing.py --load_folder /home/a09v98/imagenet/train --save_folder /home/a09v98/Projects/data/imagenet-drawing4/train --drawing-pattern /home/a09v98/Projects/imagenet-shift/src-imagenet-drawing/drawing-patterns/drawing-pattern-IV.jpg
